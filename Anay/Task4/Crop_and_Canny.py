e00fc2276c6fc2ebaa52454c19bcba0f75d8a602
Anay Khator <33987976+Anaykhator@users.noreply.github.com>
2018-12-10 04:20:20 +0530

    Update Crop_and_Canny.py

--- Anay/Task4/Crop_and_Canny.py
+++ Anay/Task4/Crop_and_Canny.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-import imutils
+#import imutils
 
 img=cv2.imread('720x1280.jpg',1)
 
@@ -8,10 +8,10 @@ img=img[:240,:]
 cv2.imshow('cropped',img)
 
 m = np.median(img)
-print(m)
+#print(m)
 lower = int(max(0,(1.0-0.33) * m))
 upper = int(min(255,(1.0+0.33) * m))
-print(lower,upper)
+#print(lower,upper)
 edged = cv2.Canny(img, lower, upper)
 
 cv2.imshow('edge_detected',edged)
